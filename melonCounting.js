e1864491da7ec81382a1584be873486e4de05652
TYLPHE <tylphet@gmail.com>
2023-04-25 21:44:41 -0600

    Completed converting python file to js

---
+++ melonCounting.js
@@ -0,0 +1,27 @@
+// Our customers are going to buy lots of melons!
+const melons_to_add = ['Ogen', 'Horned Melon', 'Watermelon', 'Casaba',
+                 'Sharlyn', 'Xigua', 'Ogen', 'Christmas', 'Christmas',
+                 'Christmas', 'Christmas', 'Watermelon', 'Sharlyn', 'Xigua',
+                 'Cantaloupe', 'Christmas', 'Watermelon', 'Christmas',
+                 'Sharlyn', 'Christmas', 'Cantaloupe', 'Casaba', 'Cantaloupe',
+                 'Santa Claus', 'Horned Melon', 'Watermelon', 'Ogen',
+                 'Horned Melon', 'Cantaloupe', 'Xigua', 'Horned Melon', 'Sharlyn',
+                 'Horned Melon', 'Sharlyn', 'Cantaloupe', 'Christmas',
+                 'Horned Melon', 'Horned Melon', 'Horned Melon', 'Xigua', 'Xigua',
+                 'Watermelon', 'Cantaloupe', 'Casaba', 'Cantaloupe', 'Casaba',
+                 'Watermelon', 'Santa Claus', 'Casaba']
+
+function count_melons(melonArr) {
+  const melonCounts = {};
+  
+  for (melon of melonArr) {
+    if (melonCounts.hasOwnProperty(melon)) {
+      melonCounts[melon] += 1
+    } else {
+      melonCounts[melon] = 1
+    }
+  }
+  return melonCounts
+}
+
+count_melons(melons_to_add)
