60b5b32b4739091d5558706916f414e5660b2a7b
TYLPHE <tylphet@gmail.com>
2023-04-25 21:46:32 -0600

    Updated array to be more readable

--- melonCounting.js
+++ melonCounting.js
@@ -1,15 +1,55 @@
 // Our customers are going to buy lots of melons!
-const melons_to_add = ['Ogen', 'Horned Melon', 'Watermelon', 'Casaba',
-                 'Sharlyn', 'Xigua', 'Ogen', 'Christmas', 'Christmas',
-                 'Christmas', 'Christmas', 'Watermelon', 'Sharlyn', 'Xigua',
-                 'Cantaloupe', 'Christmas', 'Watermelon', 'Christmas',
-                 'Sharlyn', 'Christmas', 'Cantaloupe', 'Casaba', 'Cantaloupe',
-                 'Santa Claus', 'Horned Melon', 'Watermelon', 'Ogen',
-                 'Horned Melon', 'Cantaloupe', 'Xigua', 'Horned Melon', 'Sharlyn',
-                 'Horned Melon', 'Sharlyn', 'Cantaloupe', 'Christmas',
-                 'Horned Melon', 'Horned Melon', 'Horned Melon', 'Xigua', 'Xigua',
-                 'Watermelon', 'Cantaloupe', 'Casaba', 'Cantaloupe', 'Casaba',
-                 'Watermelon', 'Santa Claus', 'Casaba']
+const melonsToAdd = [
+  'Ogen',
+  'Horned Melon',
+  'Watermelon',
+  'Casaba',
+  'Sharlyn',
+  'Xigua',
+  'Ogen',
+  'Christmas',
+  'Christmas',
+  'Christmas',
+  'Christmas',
+  'Watermelon',
+  'Sharlyn',
+  'Xigua',
+  'Cantaloupe',
+  'Christmas',
+  'Watermelon',
+  'Christmas',
+  'Sharlyn',
+  'Christmas',
+  'Cantaloupe',
+  'Casaba',
+  'Cantaloupe',
+  'Santa Claus',
+  'Horned Melon',
+  'Watermelon',
+  'Ogen',
+  'Horned Melon',
+  'Cantaloupe',
+  'Xigua',
+  'Horned Melon',
+  'Sharlyn',
+  'Horned Melon',
+  'Sharlyn',
+  'Cantaloupe',
+  'Christmas',
+  'Horned Melon',
+  'Horned Melon',
+  'Horned Melon',
+  'Xigua',
+  'Xigua',
+  'Watermelon',
+  'Cantaloupe',
+  'Casaba',
+  'Cantaloupe',
+  'Casaba',
+  'Watermelon',
+  'Santa Claus',
+  'Casaba',
+];
 
 function count_melons(melonArr) {
   const melonCounts = {};
@@ -24,4 +64,4 @@ function count_melons(melonArr) {
   return melonCounts
 }
 
-count_melons(melons_to_add)
+console.log(count_melons(melonsToAdd))
